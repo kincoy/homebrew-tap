293ab7d9a5417f008495c9142000e19d43ab0e11
kincoy <kincoyao@gmail.com>
2026-03-28 12:58:48 +0800

    fix: correct cc9s 0.2.3 checksums

--- cc9s.rb
+++ cc9s.rb
@@ -7,22 +7,22 @@ class Cc9s < Formula
   on_macos do
     on_intel do
       url "https://github.com/kincoy/cc9s/releases/download/v#{version}/cc9s-darwin-amd64"
-      sha256 "e3f65bedb6d6f55a9490b2311dd04c0bd41c3937a0ce714e43eeb191a6d0b6a2"
+      sha256 "529b7b39c3f446054c65db1f555dae0cdee52dec54801df72aa28e6e9698d2b2"
     end
     on_arm do
       url "https://github.com/kincoy/cc9s/releases/download/v#{version}/cc9s-darwin-arm64"
-      sha256 "b0f46f2e729d84e65972e089a2a03d8c46353425a200b8551bc61452f99b2367"
+      sha256 "9305b1322705b1ba8062169a6060efce20161872609bff86bab49c9007a9e95e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/kincoy/cc9s/releases/download/v#{version}/cc9s-linux-amd64"
-      sha256 "a759bf44457fa65ec86407c38754e53efffeb8de345c1945d2c3487960ca84ce"
+      sha256 "05932d4cf9dc25d958c81172a8098967e3605f1c83515fc139c959df0f38c71e"
     end
     on_arm do
       url "https://github.com/kincoy/cc9s/releases/download/v#{version}/cc9s-linux-arm64"
-      sha256 "32663c16d623fe088f3fbdc84eb1cec8c94fb987f3f8284ac387eb786e5fb7e2"
+      sha256 "8c65dd6758ff739f4ceb5910d0dc85c8bc53f4df905bc2759e327486db5983ea"
     end
   end
 
